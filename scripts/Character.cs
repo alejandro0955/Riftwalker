326869bb9b05d098c1b21c51f72b38ca1fb27d3e
alejandro0955 <33553049+alejandro0955@users.noreply.github.com>
2024-09-29 10:54:41 -0400

    some changes

--- scripts/Character.cs
+++ scripts/Character.cs
@@ -1,34 +1,59 @@
 using Godot;
-using System;
+using Godot.Collections;
 
-public partial class Character : Sprite2D
+public partial class Character : CharacterBody2D
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
+    float FRICTION = 0.15f;
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-		int baseSpeed = 5;
-		if (Input.IsKeyPressed(Key.W))
-		{
-			this.Position += new Vector2(0, -baseSpeed);
-		}
-		if (Input.IsKeyPressed(Key.S))
-		{
-			this.Position += new Vector2(0, baseSpeed);
-		}
-		if (Input.IsKeyPressed(Key.A))
-		{
-			this.Position += new Vector2(-baseSpeed, 0);
-		}
-		if (Input.IsKeyPressed(Key.D))
-		{
-			this.Position += new Vector2(baseSpeed, 0);
-		}
+    [Export]
+    int hp = 2;
 
+    [Export]
+    int acceleration = 100;
 
-	}
+    [Export]
+    int max_speed = 200;
+
+    public AnimatedSprite2D AnimatedSprite;
+    public Node StateMachine;
+
+    public override void _Ready()
+    {
+        AnimatedSprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+        StateMachine = GetNode<Node>("FiniteStateMachine");
+    }
+
+    public Vector2 mov_direction = Vector2.Zero;
+
+    public override void _PhysicsProcess(double delta)
+    {
+        MoveAndSlide();
+        Velocity = Velocity.Lerp(Vector2.Zero, FRICTION);
+    }
+
+    public void Move()
+    {
+        mov_direction = mov_direction.Normalized();
+        Velocity += mov_direction * acceleration;
+        Velocity = Velocity.LimitLength(max_speed);
+    }
+
+    public void take_damage(int dam, Vector2 dir, int force)
+    {
+        hp -= dam;
+        StateMachine = GetNode<Node>("FiniteStateMachine");
+        var States = StateMachine.Get("States");
+        Dictionary dict = States.AsGodotDictionary();
+        if (hp > 0)
+        {
+            GD.Print(States);
+            StateMachine.Call("SetState", dict["Hurt"]);
+            Velocity += dir * force;
+        }
+        else
+        {
+            StateMachine.Call("SetState", dict["Dead"]);
+            Velocity += dir * force * 2;
+        }
+    }
 }
